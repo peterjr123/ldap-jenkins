ad98e68c4f66d020102d6e224d282aa37c97eced
박준석 <jspark.dsmentoring.com>
2025-01-21 15:56:30 +0900

    add test

---
+++ src/test/java/org/example/TestMain.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestMain {
+    @Test
+    public void alwaysTrue() {
+        Assertions.assertTrue(true);
+    }
+}
